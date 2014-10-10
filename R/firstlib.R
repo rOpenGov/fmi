0f685fd9979ec1df48151ecf0971cb0d64b21683
antagomir <leo.lahti@iki.fi>
2014-10-10 14:32:19 +0200

    pkg startup msg

---
+++ R/firstlib.R
@@ -0,0 +1,9 @@
+#' @import sp
+#' @import rgdal
+
+.onAttach <- function(lib, pkg)
+{
+
+  packageStartupMessage("fmi - R Tools for Finnish Meteorological Institute Open Data.\nCopyright (C) 2014 Jussi Jousimo ym.\n\nhttps://ropengov.github.com/fmi \n")
+
+}
