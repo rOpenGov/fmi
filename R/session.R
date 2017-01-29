19e5c17f2df76732b17a8bc6c3f887b57c17c32f
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-01-29 11:48:07 +0100

    Document init_session()

--- R/session.R
+++ R/session.R
@@ -20,8 +20,20 @@ check_session <- function() {
     stop("No session initialized, see init_session()")
   }
 }
-
+#' Initialize a API session.
+#' 
+#' Creates a new session based on a provided API key. Function creates a new
+#' \code{\link{FMIWFSClient}} object and assigns it to a environment called
+#' \code{client}.
+#' 
+#' @seealso \code{\link[rwfs]{WFSClient}}, \code{\link[rwfs]{WFSCachingClient}}
+#' 
+#' @param apikey Character string valid key for the FMI API.
+#' 
+#' @author Joona Lehtomaki  \email{joona.lehtomaki@gmail.com}
+#' @examples # See the vignette.
 #' @export
+#' 
 init_session <- function(apikey) {
   client <- FMIWFSClient$new(
     FMIWFSRequest$new(apiKey = apikey)
